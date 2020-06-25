ea83be1682a7e127d494a8c55e769fb37126c594
Alex <rabbitx98@gmail.com>
2020-06-25 05:11:32 +0430

    add all files

---
+++ bot.py
@@ -0,0 +1,63 @@
+import pyautogui   # pip3 install pyautogui
+import time
+
+
+def get_data_and_posetion(): 
+    """
+    use this function to get the telegram text areat position
+
+    return :pos_x  X pos of courser
+           :pos_y  Y pos of courser
+    """
+
+    print("you have 5 sec to point the courser to telegram input text pos")
+    time.sleep(5)
+    pos_x,pos_y = pyautogui.position()
+    return  pos_x,pos_y
+
+
+
+
+
+def run():
+    """
+    this function use get_data_and_position to get palce that bot want to write
+
+    also get the text and count.
+
+    """
+    text = input('gimme yor text in english: > ')
+    count = input('how many send : > ')
+    pos_x,pos_y = get_data_and_posetion() 
+    steps_count = len([i for i in range(0,int(count),50)])
+    fif_count = steps_count * .4 
+    print("bot is started")
+    print('')
+    print(f'it will take {int(fif_count)} sec to finish! ')
+    for i in range(int(count)):
+        # every 50 times wait for 0.4 sec
+        if i % 50 == 0:
+            time.sleep(.4)
+        
+        # every relatively count, print a log     
+        if i % 10**(len(str(i//10))) == 0:
+            remain = int(count) - i 
+            print(f"{remain} more to go :D" )
+
+        # do the job here
+        # pyautogui.moveTo(pos_x,pos_y) 
+        pyautogui.click(pos_x,pos_y) 
+        pyautogui.typewrite(text)  
+        pyautogui.hotkey("enter")
+    
+    print('')
+    print('hey im Done!')
+
+
+if __name__ == "__main__":
+    run()
+    print('')
+    print('_______________________')
+    print("yo! join @rabbitix :D")
+    print('_______________________')
+
--- readme.md
+++ readme.md
@@ -11,3 +11,11 @@ how to use it?
 run the `bot.py` file with this command:
 > `python bot.py`
 
+
+
+### Notice 
+__i do not responsible for any use of this litle script!!__
+
+**if you want to stop bot when you want, try to move mouse to top left position**
+
+
